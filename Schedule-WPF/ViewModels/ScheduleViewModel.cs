c652840b57fbd51e593f1fcfdd2c6eadc08376e6
Artur Sizov <artur-sa@mail.ru>
2023-06-15 21:46:35 +0300

    Добавил отображение текущей даты.

--- Schedule-WPF/ViewModels/ScheduleViewModel.cs
+++ Schedule-WPF/ViewModels/ScheduleViewModel.cs
@@ -1,10 +1,14 @@
-﻿namespace Schedule_WPF.ViewModels
+﻿using System;
+using System.Windows.Controls;
+
+namespace Schedule_WPF.ViewModels
 {
     public class ScheduleViewModel
     {
 
         #region Public property 
         public string Title => "Schedule";
+        public DateTime DateTimeNow => DateTime.Now;
         #endregion
     }
 }
