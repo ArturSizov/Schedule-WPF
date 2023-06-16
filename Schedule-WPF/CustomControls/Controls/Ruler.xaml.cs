ca15a070681834417b9660685fcff245b2556bc4
Artur Sizov <artur-sa@mail.ru>
2023-06-16 19:49:21 +0300

    Начал работу над линейкой

---
+++ Schedule-WPF/CustomControls/Controls/Ruler.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Schedule_WPF.CustomControls.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для Ruler.xaml
+    /// </summary>
+    public partial class Ruler : Grid
+    {
+        public Ruler()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Schedule-WPF/ViewModels/ScheduleViewModel.cs
+++ Schedule-WPF/ViewModels/ScheduleViewModel.cs
@@ -1,14 +1,26 @@
-﻿using System;
+﻿using Prism.Mvvm;
+using System;
 using System.Windows.Controls;
 
 namespace Schedule_WPF.ViewModels
 {
-    public class ScheduleViewModel
+    public class ScheduleViewModel : BindableBase
     {
+
+        #region Private property 
+        private int pending = 12;
+        private int jeopardy;
+        private int completed;
+
+        #endregion
 
         #region Public property 
         public string Title => "Schedule";
         public DateTime DateTimeNow => DateTime.Now;
+        public int Pending { get => pending; set => SetProperty(ref pending, value); }
+        public int Jeopardy { get => jeopardy; set => SetProperty(ref jeopardy, value); }
+        public int Completed { get => completed; set => SetProperty(ref completed, value); } 
+
         #endregion
     }
 }
