221f1e4129cb52b11ebf586ce5ce8b572b423e81
Artur Sizov <artur-sa@mail.ru>
2023-06-19 20:42:19 +0300

    Работа над логикой линейки завершена

--- Schedule-WPF/CustomControls/Controls/Ruler.xaml.cs
+++ Schedule-WPF/CustomControls/Controls/Ruler.xaml.cs
@@ -1,15 +1,84 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace Schedule_WPF.CustomControls.Controls
 {
-    /// <summary>
-    /// Логика взаимодействия для Ruler.xaml
-    /// </summary>
     public partial class Ruler : Grid
     {
+        #region Private propery 
+        private int count = 0;
+        #endregion
+
+        #region Public property 
+        public string Number
+        {
+            get => (string)GetValue(NumberProperty);
+            set
+            {
+                SetValue(NumberProperty, value); 
+            }
+        }
+        #endregion
+
+
+        #region DependencyProperty
+        public static readonly DependencyProperty NumberProperty = DependencyProperty.Register(nameof(Number), typeof(string), typeof(Ruler),
+        new PropertyMetadata(NumberPropertyChanged));
+        #endregion
         public Ruler()
         {
             InitializeComponent();
         }
+
+        #region Methods 
+        private static void NumberPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((Ruler)d).NumberPropertyChanged();
+        }
+
+        private void NumberPropertyChanged()
+        {
+            var n = Convert.ToInt32(Number) / 5;
+
+            if (n == 0) throw new Exception("Ruler value 0");
+
+            for (int i = 0; i < n; i++)
+            {
+                sp.Children.Add(CreateRoler());
+                count = count + 5;
+            }
+        }
+        private Line CreateLine(int x1, int y1, int x2, int y2)
+        {
+            var line = new Line();
+            line.X1 = x1;
+            line.Y1 = y1;
+            line.X2 = x2;
+            line.Y2 = y2;
+            line.Stroke = Brushes.Black;
+            return line;
+        }
+
+        private Grid CreateRoler()
+        {
+            var grid = new Grid();
+            var text = new TextBlock();
+            text.Margin = new Thickness(2, -5, 0, 0);
+            text.Text = $"{count}";
+            grid.Children.Add(text);
+            grid.Children.Add(CreateLine(0, 15, 125, 15));
+            grid.Children.Add(CreateLine(0, -5, 0, 15));
+            grid.Children.Add(CreateLine(25, 7, 25, 15));
+            grid.Children.Add(CreateLine(50, 7, 50, 15));
+            grid.Children.Add(CreateLine(75, 7, 75, 15));
+            grid.Children.Add(CreateLine(100, 7, 100, 15));
+            grid.Children.Add(CreateLine(125, 0, 125, 0));
+
+            return grid;
+        }
+        #endregion
     }
 }
